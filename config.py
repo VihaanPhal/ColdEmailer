961c71d71b8569ef6ac7ddd94e0b1f868c9f816b
Vihaan Phal <phal.vihaan@gmail.com>
2025-02-26 11:35:55 -0700

    added resume

--- config.py
+++ config.py
@@ -5,4 +5,4 @@ load_dotenv()
 
 SENDER_EMAIL = os.getenv("SENDER_EMAIL")
 SENDER_PASSWORD = os.getenv("SENDER_PASSWORD")
-RESUME_PATH = "static/resume.pdf"
+RESUME_PATH = "static/VihaanPhal_Fulltime.pdf"
